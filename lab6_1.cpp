2ce1ac0f782fee5478b37b546c49c4849fd9796e
Chinoros <chinoros2815@gmail.com>
2024-11-29 15:37:33 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,17 @@
 using namespace std;
 
 int main(){
+    int x, even = 0, odd = 0;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> x;
+    while (x != 0) {
+        if (x % 2 == 0) {
+            even++;
+        } else {odd++;}
+        cout << "Enter an integer: ";
+        cin >> x;
+    }
+    cout << "#Even numbers = " << even << '\n';
+    cout << "#Odd numbers = " << odd;
     return 0;
 }
